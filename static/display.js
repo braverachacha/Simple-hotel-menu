cd44ceefe49304bc06a17c571f777e669aee1207
braverachacha <braverachacha@gmail.com>
2025-10-17 14:32:56 +0300

    Added javascript for interactivity and changed some layout

--- static/display.js
+++ static/display.js
@@ -1,13 +1,43 @@
 const displayElement = document.querySelector('.products-section');
 
 displayElement.innerHTML = '';
-products.forEach((items) =>{
+products.forEach((item) =>{
 	displayElement.innerHTML += `
 		<div class="products">
-	    	<img src="${items.image}">
-	        <p class="product-name"> Food : ${items.name}</p>
-	        <p class="price"> Price : ${items.price}</p>
-	        <button>Place Order</button>
+	    	<img src="${item.image}">
+	        <p class="product-name"> ${item.name}</p>
+	        <p class="price"> Ksh : ${item.price}</p>
+	        <button data-product-id="${item.id}" class="js-place-order">Place Order</button>
 		</div>
 	`
 });
+
+document.querySelectorAll('.js-place-order')
+  .forEach((button) =>{
+    button.addEventListener('click', ()=>{
+     const productId =  button.dataset.productId;
+     
+     let matchingItem;
+     cart.forEach((order) =>{
+       if(productId === order.productId){
+         matchingItem = order;
+       }
+     });
+     if(matchingItem){
+       matchingItem.quantity += 1;
+     } else {
+       cart.push({
+         productId : productId,
+         quantity : 1
+       });
+     }
+     
+     let cartQuantity = 0;
+     cart.forEach((orders) =>{
+      cartQuantity += orders.quantity;
+     })
+     document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+    });
+  });
+  
--- static/products.js
+++ static/products.js
@@ -1,21 +1,51 @@
 const products = [{
+  id : '12dr3-pepsi-34yu',
 	image : 'static/pepsi.jpg',
 	name : 'One 400ml Pepsi Can',
 	price : 250,
 },{
+  id : '222g-juice-45ed',
 	image : 'static/pizza-drink.jpg',
 	name : 'Pizza and Juice one glass',
 	price : 3250,
 },{
+  id : '9jb8-pizza-iu43',
 	image : 'static/pizza.jpg',
 	name : '1kg Pizza',
 	price : 3200,
 },{
+  id : '45wg-burger-5gh6',
 	image : 'static/bugger.jpg',
 	name : '3 Pieces of Burger',
 	price : 1990,
 },{
+  id : 're5h-chicken-kdf6',
 	image : 'static/fried-chicken.jpg',
 	name : 'Fried Chicken',
 	price : 2950,
+},{
+  id : 'rr5h-Salad-kd96',
+	image : 'static/Garden-Fresh-Salad.jpg',
+	name : 'Garden Fresh Salad',
+	price : 950,
+},{
+  id : 'kr5h-Mushroom-kj96',
+	image : 'static/Creamy-Mushroom-Soup.jpg',
+	name : 'Creamy Mushroom Soup',
+	price : 590,
+},{
+  id : 'rr5h-Salad-kd96',
+	image : 'static/Grilled-Chicken-with-Rice.jpg',
+	name : 'Grilled Chicken with Rice',
+	price : 910,
+},{
+  id : 'r4th-Potatoes-kh91',
+	image : 'static/Goat Chops with Mashed Potatoes.jpg',
+	name : 'Goat Chops with Mashed Potatoes',
+	price : 730,
+},{
+  id : 'r4th-Spaghetti-kh91-ty78',
+	image : 'static/Spaghetti Bolognese.jpg',
+	name : 'Spaghetti Bolognese',
+	price : 840,
 }]
